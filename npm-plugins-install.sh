3cf7aa12ce5f8cd6c58b44652a57c6b0fc788204
Gabriel Russo <tenbu64@proton.me>
2024-06-03 22:09:15 -0300

    add script to install dependencies

---
+++ npm-plugins-install.sh
@@ -0,0 +1,12 @@
+#!bin/fish
+echo "installing"
+sudo npm i -g vscode-langservers-extracted --noconfirm
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+sudo npm i -g css-variables-language-server
+sudo npm i -g sql-language-server
+sudo pacman -S nix --noconfirm
+sudo pacman -S fuzzel --noconfirm
+sudo pacman -R wofi --noconfirm
+sudo pacman -S fnott --noconfirm
+yay -S swayrst-git --noconfirm
