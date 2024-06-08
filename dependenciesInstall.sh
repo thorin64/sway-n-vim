8fc990b3f6f1cfa6944bd89d049ae175c2cfb938
Gabriel Russo <tenbu64@proton.me>
2024-06-07 22:02:37 -0300

    update sh

--- dependenciesInstall.sh
+++ dependenciesInstall.sh
@@ -8,4 +8,4 @@ sudo npm i -g sql-language-server
 sudo pacman -R wofi --noconfirm
 yay -S swayrst-git --noconfirm
 yay -S swww --noconfirm
-sudo pacman -S nix fuzzel mako fnottsudo ttf-nerd-fonts-symbols-m ttf-nerd-fonts-symbols ttf-terminus-nerd --noconfirm
+sudo pacman -S nix fuzzel mako ttf-nerd-fonts-symbols ttf-terminus-nerd --noconfirm
