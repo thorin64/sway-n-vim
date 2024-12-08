d3d0cd22d68c44498f55cfdd208e4274fbf0b297
Gabriel Russo <tenbu64@proton.me>
2024-12-08 16:34:53 -0300

    updating stuff

--- dependenciesInstall.sh
+++ dependenciesInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/fish
 echo "installing"
+sudo pacman -S bash-language-server cmus cargo npm nix fuzzel mako ttf-nerd-fonts-symbols ttf-terminus-nerd --noconfirm
 sudo npm i -g vscode-langservers-extracted --noconfirm
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 sudo npm i -g css-variables-language-server
 sudo npm i -g sql-language-server
 sudo pacman -R wofi --noconfirm
-yay -S swayrst-git --noconfirm
-yay -S swww --noconfirm
-sudo pacman -S nix fuzzel mako ttf-nerd-fonts-symbols ttf-terminus-nerd --noconfirm
+yay -Sy swayrst-git tuime --noconfirm
+
--- pull-script.sh
+++ pull-script.sh
@@ -1,11 +1,12 @@
 #!/bin/fish
 
-cp -rf ~/.config/fish ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/lazygit ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/nvim ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/ranger ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/sway ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/waybar ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/foot ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/mako ~/Projects/linuxStuff/sway-n-vim/
-cp -rf ~/.config/fuzzel ~/Projects/linuxStuff/sway-n-vim/
+cp -rf ~/.config/fish ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/wayvnc ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/cmus ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/nvim ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/ranger ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/sway ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/waybar ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/foot ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/mako ~/Projects/linuxStuff/sway-n-vim/.config
+cp -rf ~/.config/fuzzel ~/Projects/linuxStuff/sway-n-vim/.config
