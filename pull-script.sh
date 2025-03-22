b86153bbd3407d82600ec14f7dc3bb874dbf9471
Gabriel Russo <tenbu64@proton.me>
2025-03-22 20:18:40 -0300

    removing .local

--- pull-script.sh
+++ pull-script.sh
@@ -13,4 +13,4 @@ cp -rf ~/.config/fuzzel ~/Projects/linuxStuff/sway-n-vim/.config
 cp -rf ~/.config/stmp/ ~/Projects/linuxStuff/sway-n-vim/.config
 cp -rf ~/.config/calcurse/ ~/Projects/linuxStuff/sway-n-vim/.config
 
-cp -rf ~/.local/share/ranger ~/Projects/linuxStuff/sway-n-vim/.local/share
+# cp -rf ~/.local/share/ranger ~/Projects/linuxStuff/sway-n-vim/.local/share
